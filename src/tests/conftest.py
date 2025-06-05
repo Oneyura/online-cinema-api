93c31ab9755975a42e0a9b6022dc806950c71246
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-06-05 09:49:50 +0300

    add annotation test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
+from fastapi import FastAPI
 from src.main import app
 
 @pytest.fixture
@@ -10,7 +11,7 @@ def client() -> TestClient:
     return TestClient(app)
 
 @pytest.fixture
-def test_app():
+def test_app() -> FastAPI:
     """
     Create a test instance of the FastAPI application.
     """
