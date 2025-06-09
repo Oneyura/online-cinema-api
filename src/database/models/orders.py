b94ea985ed1d61dc6857394128cdf4ed45fbbfb2
Nikita-pyth <siorito321@gmail.com>
2025-06-09 20:08:45 +0300

    added backward relation to movie in order_item

--- src/database/models/orders.py
+++ src/database/models/orders.py
@@ -38,5 +38,6 @@ class OrderItem(Base):
     price_at_order = Column(Numeric(10, 2), nullable=False)
 
     order = relationship("Order", back_populates="items")
+    movie = relationship("Movie", back_populates="order_items")
     payments_items: Mapped[List["PaymentsItem"]] = relationship("PaymentsItem", back_populates="order_item")
 
